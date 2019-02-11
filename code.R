81f0542163666c79e24a8242eb09369332f1c76d
DanielQuiroz97 <cristianquirozd1997@gmail.com>
2019-02-11 15:25:57 -0500

    bar plot

--- code.R
+++ code.R
@@ -2,6 +2,8 @@
 library(tidyverse) # Tidy data
 library(magrittr)  # Pipes
 library(readxl)    # Read data
+library(ggsignif) 
+library(ggpubr)
 
 #### Importing Data ####
 p_vulgaris <- read_xlsx('data.xlsx', sheet = "all_data") %>% 
@@ -14,6 +16,12 @@ p_vulgaris <- read_xlsx('data.xlsx', sheet = "all_data") %>%
 
 
 #### Plots ####
-p_vulgaris %>% ggplot(aes(Group, Value, fill = Group)) + geom_boxplot() +
-  facet_wrap('Variable', scales = 'free_x', ncol = 2) + theme_bw() +
-  coord_flip() 
+p_vulgaris %>% ggplot(aes(Group, Value, color = Group)) + 
+  geom_boxplot(alpha = 0.6) +
+  geom_point(aes(color = Group), size = 0.8) +
+  facet_wrap('Variable', scales = 'free_y', ncol = 2) + theme_cleveland() +
+  stat_compare_means(method = "anova") +
+  labs(color = "Tratamiento") +
+  scale_color_discrete(labels=c("Control", "Treatment 1", "Treatment 2"))
+
+
